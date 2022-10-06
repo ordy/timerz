b20a1a9be3b53c7e838fd732a5345162fc1b7427
Sandro T <ordysan@gmail.com>
2022-10-06 19:37:54 +0200

    created navbar and colorscheme

---
+++ components/navbar.js
@@ -0,0 +1,10 @@
+import { Appbar } from 'react-native-paper';
+
+const Navbar = () => (
+	<Appbar.Header>
+		<Appbar.Content title='Timerz' />
+		<Appbar.Action icon='dots-vertical' onPress={() => {}} />
+	</Appbar.Header>
+);
+
+export default Navbar;
---
+++ constants/colors.js
@@ -0,0 +1,10 @@
+export const COLORS = {
+	pBG: '#f5f5f6',
+	sBG: '#e1e2e1',
+	primary: '#14213d',
+	pDark: '#000018',
+	pLight: '#3e4868',
+	secondary: '#fca311',
+	sDark: '#c37400',
+	sLight: '#ffd44f',
+};
