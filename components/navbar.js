a4ff922610dcd9b4cc52cc7e47b6d32f5597ca2a
Sandro T <ordysan@gmail.com>
2022-10-07 00:08:41 +0200

    themed nav bar

--- components/navbar.js
+++ components/navbar.js
@@ -1,10 +1,19 @@
 import { Appbar } from 'react-native-paper';
+import { StyleSheet } from 'react-native';
+import { COLORS } from '../constants/colors';
 
 const Navbar = () => (
-	<Appbar.Header>
-		<Appbar.Content title='Timerz' />
-		<Appbar.Action icon='dots-vertical' onPress={() => {}} />
+	<Appbar.Header style={styles.top}>
+		<Appbar.Content title='Timerz' titleStyle={{ color: COLORS.pBG }} />
+		<Appbar.Action icon='plus' onPress={() => {}} color={COLORS.pBG} />
+		<Appbar.Action icon='dots-vertical' onPress={() => {}} color={COLORS.pBG} />
 	</Appbar.Header>
 );
+
+const styles = StyleSheet.create({
+	top: {
+		backgroundColor: COLORS.primary,
+	},
+});
 
 export default Navbar;
